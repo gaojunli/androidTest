96b52932103d50b1419bade430d40fe1325ba251
“gaojunwx <15380944203@139.com>
2019-12-21 11:38:58 +0800

    second code commit

---
+++ app/src/main/java/com/test/gj/Task/MyAsyncTask.java
@@ -0,0 +1,20 @@
+package com.test.gj.Task;
+
+import android.os.AsyncTask;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2019/12/20
+ * @Description:
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class MyAsyncTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+
+
+
+        return null;
+    }
+}
